a3b75c72e4fa966d7046baf978014ece9c0b763a
Luis Ochoa <lalocho13@gmail.com>
2020-02-26 17:45:14 -0700

    got it to print $ and take input

--- shell.c
+++ shell.c
@@ -2,12 +2,35 @@
 #include<string.h>
 #include<stdlib.h>
 #include<unistd.h>
+#define SIZE 100
+char* takeInput(void){
+    int bufferSize = SIZE;
+    int position=0;
+    char *buffer = malloc(sizeof(char) * bufferSize);
+    int charac;
+    while(1){
+     charac = getchar();
+     if(charac == EOF || charac == '\n'){
+         buffer[position] = '\0';
+         return buffer;
+     }else{
+      buffer[position] == charac;   
+     }
+     position+1;
+    }
 
-int main(){
+}
+int main(int argc, char **argv){
+  
+  char* prompt;
   while(1){
-    printf("$");
+    prompt = getenv("PS1");
+    if(!prompt){
+        printf("$");
+    }else{
+    printf("%s",prompt);
+    }
+    char* input = takeInput();
   }
 }
-int takeInput(char* str){
 
-}
