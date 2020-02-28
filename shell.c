b942ae4808cde310649e5b246e67183736b12e2f
Luis Ochoa <lalocho13@gmail.com>
2020-02-27 19:10:32 -0700

    got normal commands to work

--- shell.c
+++ shell.c
@@ -2,63 +2,18 @@
 #include<string.h>
 #include<stdlib.h>
 #include<unistd.h>
-#include<sys/wait.h>
 #include<fcntl.h>
-#define SIZE 100
-//#define ESCAPE_CHARACTER " \t\r\a\n"
-int redirect();
-char* takeInput();
-char** tokenizer();
-
-
-char* takeInput(void){
-    
-    int position=0;
-    char *buffer = malloc(sizeof(char) * SIZE);
-    int charac;
-    while(1){
-     charac = getchar();
-     if(charac == EOF || charac == '\n'){
-         buffer[position] = '\0';
-         return buffer;
-     }else{
-      buffer[position] == charac;   
-     }
-     position+1;
-    }
-
-}
-char** tokenizer(char* input){
-  //int buffersize = SIZE;
-  int index= 0;
-  int charac;
-  char** word_array = malloc(sizeof(char) * SIZE);
-  char* word;
-
-  word = strtok(input," \t\a\r\n");
-      
-  while(word){
-    word_array[index] = word;
-    index ++;
-    word = strtok(NULL, "\t\a\r\n");
-    }
-  word_array[index] = NULL;
-  return word_array;
-}
-
-int redirect(){
+#include<sys/wait.h>
+char** parser(char* input);
+int redirect(char** myargs);
+int exe(char** myargs){
   int rc = fork();
   if(rc < 0){
     fprintf(stderr, "Fork failed\n");
     exit(1);
   }
   else if(rc == 0){
-    close(SDTOUT_FILENO);
-    open("./p4-output.txt", O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
-    char *myargs[3];
-    myargs[0] = strdup("WC");
-    myargs[1] = strdup("redirect.c");
-    myargs[2] = NULL;
+    
     execvp(myargs[0], myargs);
   }
   else{
@@ -68,17 +23,65 @@ int redirect(){
 
 }
 
-int main(int argc, char *argv[]){
-  
-  char* prompt;
-  while(1){
+int main(){
+  char *command;
+  char **parsedCommand;
+  int status;
+  char *prompt;
+  do {
     prompt = getenv("PS1");
     if(!prompt){
         printf("$");
     }else{
     printf("%s",prompt);
     }
-    char* input = takeInput();
-  }
-}
+    //reads input
+    ssize_t bufsize=0;
+    getline(&command, &bufsize, stdin);
+    printf("%s",command);
+    parsedCommand = parser(command);
+    if(strcmp(parsedCommand[0],"exit") == 0){
+        break;
+    }else if(strcmp(parsedCommand[0] , "cd") == 0){
+        //redirect(parsedCommand);
+    }else if(strcmp(parsedCommand[0] , ">") == 0){
+        break;
+    }else if(strcmp(parsedCommand[0] , "|") == 0){
+        break;
+    }else{
+        exe(parsedCommand);
+    }
+    
 
+    free(command);
+    //free(args);
+  } while (1);
+}
+void changeDir(char** input){
+    
+}
+void redirectIO(char** input){
+    
+}
+void pipeIT(char** input){
+    
+}
+#define SIZE  100
+#define ESCAPE_CHARACTERS " \a\n\r\t"
+char** parser(char* input){
+  int index= 0;
+  char** word_array = malloc(sizeof(char) * (int)SIZE);
+  char* word;
+
+  word = strtok(input,ESCAPE_CHARACTERS);
+  
+  while(word!= NULL){
+    printf("%s",word);
+    word_array[index] = word;
+    index ++;
+    word = strtok(NULL, ESCAPE_CHARACTERS);
+    
+    }
+  word_array[index] = NULL;
+  return word_array;
+}
